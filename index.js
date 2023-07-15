6380c205d0dc9a409b715a81df3ec417b3b51405
Tyler Varzeas <tyler.varzeas@gmail.com>
2023-07-14 21:33:22 -0700

    Mongoose Part 3

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
-    useNewUrlParse: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 connect.then(() => {
-
+    
     console.log('Connected correctly to server');
 
     Campsite.create({
@@ -18,7 +18,23 @@ connect.then(() => {
     })
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+
+        return Campsite.findByIdAndUpdate(cmapsite._id, {
+            $set: { description: 'Updated Test Document' }
+        }, {
+            new: true
+        });
+    })
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save();
     })
     .then(campsite => {
         console.log(campsite);
--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 const campsiteSchema = new Schema({
     name: {
         type: String,
